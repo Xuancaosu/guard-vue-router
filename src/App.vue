51eda05be2883f78cd9857402c4e6fbe40849493
Lucsun <suntianxiang@authing.cn>
2022-07-05 11:08:28 +0800

    init code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <h2>{{$route.name}}</h2>
   </div>
   <router-view/>
 </template>
 
 <style>
-#app {
+#app #nav {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  padding: 30px;
+
 }
 
-#nav {
-  padding: 30px;
-}
 
 #nav a {
   font-weight: bold;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <Guard appId="5f6265c67ff6fdae64ec516e" @login="onLogin"></Guard>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Guard } from '@authing/vue-ui-components'
+import "@authing/vue-ui-components/lib/index.min.css";
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Guard
+  },
+  methods: {
+    onLogin() {
+      console.log('login')
+      this.$router.push('/about')
+    }
   }
 }
 </script>
